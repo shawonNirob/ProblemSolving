8d63104bf975ea76b3fc29cc178e66a691a308ce
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-06-30 12:30:46 +0600

    Update 104. Maximum Depth of Binary Tree.java

--- leetcode/104. Maximum Depth of Binary Tree.java
+++ leetcode/104. Maximum Depth of Binary Tree.java
@@ -22,3 +22,27 @@ class Solution {
         return Math.max(left, right)+1;
     }
 }
+
+//
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null) return 0;
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(root);
+        
+        int level =0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i=0; i<size; ++i){
+                TreeNode node =queue.poll();
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            level++;
+        }
+        return level;
+    }
+}
+
+//
