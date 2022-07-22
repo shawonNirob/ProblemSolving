a21620c159509063655e5420b3974aae0e8b12f4
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-07-22 18:08:56 +0600

    Update 144. Binary Tree Preorder Traversal.java

--- leetcode/144. Binary Tree Preorder Traversal.java
+++ leetcode/144. Binary Tree Preorder Traversal.java
@@ -32,3 +32,36 @@ class Solution {
         return list;
     }
 }
+
+//Morris preorder Traverse
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root==null) return list;
+        
+        TreeNode curr = root;
+        TreeNode pre;
+        
+        while(root != null){
+            if(root.left == null){
+                list.add(root.val);
+                root = root.right;
+            }else{
+                pre = root.left;
+                while(pre.right != null && pre.right != root){
+                    pre = pre.right;
+                }
+                if(pre.right == root){
+                    root = root.right;
+                    pre.right = null;
+                }else{
+                    list.add(root.val);
+                    pre.right = root;
+                    root = root.left;
+                }
+            }
+        }
+        return list;
+    }
+}
