d6ac88271291c2d7866b142bec475826b6f02e81
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-07-31 07:48:28 +0600

    Update 98. Validate Binary Search Tree.java

--- leetcode/98. Validate Binary Search Tree.java
+++ leetcode/98. Validate Binary Search Tree.java
@@ -17,3 +17,26 @@ class Solution {
 }
 
 //
+
+class Solution {
+    public Integer prev = null;
+    public boolean isValidBST(TreeNode root) {
+       Stack<TreeNode> stack = new Stack();
+       TreeNode curr = root;
+        
+        while(curr != null || !stack.isEmpty()){
+            while(curr != null){
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            if(prev != null && curr.val <= prev) return false;
+            
+            prev = curr.val;
+            curr = curr.right;
+        }
+        return true;
+    }
+}
+
+
