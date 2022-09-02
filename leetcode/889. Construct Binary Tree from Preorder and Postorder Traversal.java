5b914a02f3726b5735b56cef0dc2ad34e06c3610
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-09-03 01:00:42 +0600

    Update 889. Construct Binary Tree from Preorder and Postorder Traversal.java

--- leetcode/889. Construct Binary Tree from Preorder and Postorder Traversal.java
+++ leetcode/889. Construct Binary Tree from Preorder and Postorder Traversal.java
@@ -44,5 +44,23 @@ class Solution {
     }
 }
 //
+class Solution {
+      public TreeNode constructFromPrePost(int[] pre, int[] post) {
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        deque.offer(new TreeNode(pre[0]));
+        for (int i = 1, j = 0; i < pre.length; ++i){
+            TreeNode root = new TreeNode(pre[i]);
+            while (deque.getLast().val == post[j]) {
+                deque.pollLast(); j++;
+            }
+            if (deque.getLast().left == null) deque.getLast().left = root;
+            else deque.getLast().right = root;
+            deque.offer(root);
+        }
+        return deque.getFirst();
+    }
+}
+
+//
 
 
