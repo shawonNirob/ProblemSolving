cf4e146b0f037bcae45ffdfaead9ac1026848a7e
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-09-11 03:59:26 +0600

    Update 268. Missing Number.java

--- leetcode/268. Missing Number.java
+++ leetcode/268. Missing Number.java
@@ -9,3 +9,17 @@ class Solution {
         return sum - sumOnly;
     }
 }
+
+//
+
+class Solution {
+    public int missingNumber(int[] nums){
+        //For example, given an input int[] nums = [3, 0, 1] which has a length of 3. The value of an int[] with length should be : 0~2. However, because of the missing element 2, there is an extra element 3 which equals nums.length takes place 2's position. Only when xor all element in nums and its index including nums.length, you can finally come up with the missing element: 3^0^1^0^1^2^3 = 2
+        
+        int ans = nums.length;
+        for(int i=0; i<nums.length; i++){
+            ans = ans ^ i ^ nums[i];
+        }
+        return ans;
+    }
+}
