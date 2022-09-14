47bde6ebfc97cbb5bc2efabc953cf2856cbc58f9
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-09-14 14:13:30 +0600

    Update 297. Serialize and Deserialize Binary Tree.java

--- leetcode/297. Serialize and Deserialize Binary Tree.java
+++ leetcode/297. Serialize and Deserialize Binary Tree.java
@@ -39,3 +39,44 @@ public class Codec {
 
 //
 
+public class Codec {
+    // Encodes a tree to a single string.
+    String ser;
+    public String serialize(TreeNode root) {
+        ser = "";
+        helper(root);
+        return ser;
+    }
+    public void helper(TreeNode root){
+        if(root==null){
+            ser += "#,";
+            return;
+        }
+        ser += String.valueOf(root.val) + ",";
+        helper(root.left);
+        helper(root.right);
+ 
+    }
+
+    // Decodes your encoded data to tree.
+    int index = 0;
+    public TreeNode deserialize(String data) {
+        String[] arr = data.split(",");
+        return createTree(arr);
+    }
+    public TreeNode createTree(String[] arr){
+        if(arr[index].equals("#")){
+            index++;
+            return null;
+        }
+        TreeNode root = new TreeNode(Integer.valueOf(arr[index]));
+        index++;
+        root.left = createTree(arr);
+        root.right = createTree(arr);
+
+        return root;
+    }
+}
+
+
+//
