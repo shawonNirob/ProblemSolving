2e3a9511a41229e21f4293404f0a37770adff7b1
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-09-15 04:45:10 +0600

    Update 449. Serialize and Deserialize BST.java

--- leetcode/449. Serialize and Deserialize BST.java
+++ leetcode/449. Serialize and Deserialize BST.java
@@ -1,3 +1,4 @@
+//1. Preorder with array
 public class Codec {
     // Encodes a tree to a single string.
     String ser;
@@ -40,7 +41,7 @@ public class Codec {
 
 
 
-//
+//2. Preorder with list
 public class Codec {
     // Encodes a tree to a single string.
     String ser;
@@ -80,4 +81,131 @@ public class Codec {
         }
     }
 }
-//
+
+//3. preorder with ArrayDeque
+public class Codec {
+    // Encodes a tree to a single string.
+    String ser;
+    public String serialize(TreeNode root) {
+        ser = "";
+        helper(root);
+        return ser;
+    }
+    public void helper(TreeNode root){
+        if(root==null){
+            return;
+        }
+        ser += String.valueOf(root.val) + ",";
+        helper(root.left);
+        helper(root.right);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if(data.length()==0) return null;
+        String[] arr = data.split(",");
+        Deque<Integer> deque = new ArrayDeque();
+        for(String s: data.split(",")){
+            deque.add(Integer.parseInt(s));
+        }
+        return createTree(deque, -1, 10001);
+    }
+    public TreeNode createTree(Deque<Integer> deque, int min, int max){
+        if(!deque.isEmpty() && min < deque.getFirst()  && max >deque.getFirst() ){
+            TreeNode root = new TreeNode(deque.getFirst());
+            
+            deque.removeFirst();
+            root.left = createTree(deque, min, root.val);
+            root.right = createTree(deque, root.val, max);
+            
+            return root;
+        }else{           
+            return null;
+        }
+    }
+}
+
+
+//4. postorder with list;
+public class Codec {
+    // Encodes a tree to a single string.
+    String ser;
+    public String serialize(TreeNode root) {
+        ser = "";
+        helper(root);
+        return ser;
+    }
+    public void helper(TreeNode root){
+        if(root==null){
+            return;
+        }
+        helper(root.left);
+        helper(root.right);
+        ser += String.valueOf(root.val) + ",";
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if(data.length()==0) return null;
+        String[] arr = data.split(",");
+        List<String> list = new ArrayList<String>(Arrays.asList(arr));
+        return createTree(list, -1, 10001);
+    }
+    public TreeNode createTree(List<String> list, int min, int max){
+        if(!list.isEmpty() && min < Integer.valueOf(list.get(list.size()-1)) && max > Integer.valueOf(list.get(list.size()-1))){
+            TreeNode root = new TreeNode(Integer.valueOf(list.get(list.size()-1)));
+            
+            list.remove(list.size()-1);
+            root.right = createTree(list, root.val, max);
+            root.left = createTree(list, min, root.val);
+            
+            return root;
+        }else{           
+            return null;
+        }
+    }
+}
+
+//5. Postorder with ArrayDeque
+public class Codec {
+    // Encodes a tree to a single string.
+    String ser;
+    public String serialize(TreeNode root) {
+        ser = "";
+        helper(root);
+        return ser;
+    }
+    public void helper(TreeNode root){
+        if(root==null){
+            return;
+        }
+        helper(root.left);
+        helper(root.right);
+        ser += String.valueOf(root.val) + ",";
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if(data.length()==0) return null;
+        String[] arr = data.split(",");
+        Deque<Integer> deque = new ArrayDeque();
+        for(String s: data.split(",")){
+            deque.add(Integer.parseInt(s));
+        }
+        return createTree(deque, -1, 10001);
+    }
+    public TreeNode createTree(Deque<Integer> deque, int min, int max){
+        if(!deque.isEmpty() && min < deque.getLast()  && max >deque.getLast() ){
+            TreeNode root = new TreeNode(deque.getLast());
+            
+            deque.removeLast();
+            root.right = createTree(deque, root.val, max);
+            root.left = createTree(deque, min, root.val);
+            
+            return root;
+        }else{           
+            return null;
+        }
+    }
+}
+
