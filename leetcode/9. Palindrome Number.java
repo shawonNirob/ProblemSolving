84a5b1c4ceac91b3012634ee10efe96daa581d5e
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-09-27 20:29:39 +0600

    Update 9. Palindrome Number.java

--- leetcode/9. Palindrome Number.java
+++ leetcode/9. Palindrome Number.java
@@ -31,3 +31,19 @@ class Solution {
 }
 
 //
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int rem = 0, num=0;
+        while(num < x){
+            num = num*10 + x%10;
+            x /= 10;
+        }
+        
+        if(x == num || x == num/10) return true;
+        else return false;
+    }
+}
+
+//
