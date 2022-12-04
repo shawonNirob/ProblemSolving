3f1e2a8e6a4f222f206668095df3d3c3e88c6e33
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-12-04 23:06:47 +0600

    Update 90. Subsets II.java

--- leetcode/90. Subsets II.java
+++ leetcode/90. Subsets II.java
@@ -1,3 +1,25 @@
+//Right 
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans = new ArrayList();
+        Arrays.sort(nums);
+        helper(0, nums, ans, new ArrayList());
+        return ans;
+    }
+    public void helper(int index, int[] arr,List<List<Integer>> ans, List<Integer> list){
+        ans.add(new ArrayList(list));
+
+        for(int i=index; i<arr.length; i++){
+            if(i > index && arr[i] == arr[i-1]) continue;
+
+            list.add(arr[i]);
+            helper(i+1, arr, ans, list);
+            list.remove(list.size()-1);
+        }
+    }
+}
+
+
 
 //Wrong Answer
 class Solution{
