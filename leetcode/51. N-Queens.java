934cd48194c3812dd9bcf6f669f667a34f62859e
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-12-12 13:12:21 +0600

    Update 51. N-Queens.java

--- leetcode/51. N-Queens.java
+++ leetcode/51. N-Queens.java
@@ -62,3 +62,52 @@ class Solution {
         return list;
     }
 }
+
+//Optimize using Hashing
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> ans = new ArrayList();
+        char[][] board =  new char[n][n];
+        for(int i=0; i< n; i++){
+            for(int j=0; j<n; j++){
+                board[i][j] = '.';
+            }
+        }
+        int[] leftRow = new int[n];
+        int[] upperDiagonal = new int[2*n - 1];
+        int[] lowerDiagonal = new int[2*n - 1];
+
+        helper(0, board,leftRow, upperDiagonal, lowerDiagonal, ans);
+        return ans;
+    }
+
+    public void helper(int col, char[][] board, int[] leftRow, int[] upperDiagonal, int[] lowerDiagonal, List<List<String>> ans){
+        if(col == board.length){
+            ans.add(construct(board));
+            return;
+        }
+        for(int row=0; row< board.length; row++){
+            if(leftRow[row]== 0 && upperDiagonal[board.length-1+col-row]==0 && lowerDiagonal[row+col]==0){
+                board[row][col] = 'Q';
+                leftRow[row]= 1;
+                upperDiagonal[board.length-1+col-row] = 1;
+                lowerDiagonal[row+col] = 1;
+                helper(col+1, board, leftRow, upperDiagonal, lowerDiagonal, ans);
+                board[row][col] = '.';
+                leftRow[row] = 0;
+                upperDiagonal[board.length-1+col-row] = 0;
+                lowerDiagonal[row+col] = 0;
+            }
+        }
+    }
+    public List<String> construct(char[][] board){
+        List<String> list = new ArrayList();
+        for(int i=0; i<board.length; i++){
+            String s = new String(board[i]);
+            list.add(s);
+        }
+        return list;
+    }
+}
+
+//
