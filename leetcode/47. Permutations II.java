9916bc37918789d879ac30cd1b619d51d1145be3
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-12-17 14:07:51 +0600

    Update 47. Permutations II.java

--- leetcode/47. Permutations II.java
+++ leetcode/47. Permutations II.java
@@ -1,50 +1,34 @@
 class Solution {
-
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> results = new ArrayList<>();
-
-        // count the occurrence of each number
-        HashMap<Integer, Integer> counter = new HashMap<>();
-        for (int num : nums) {
-            if (!counter.containsKey(num))
-                counter.put(num, 0);
-            counter.put(num, counter.get(num) + 1);
+        List<List<Integer>> ans = new ArrayList();
+        HashMap<Integer,Integer> map = new HashMap();
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i], 1);
+            }else{
+                map.put(nums[i], map.get(nums[i])+1);
+            }
         }
-
-        LinkedList<Integer> comb = new LinkedList<>();
-        this.backtrack(comb, nums.length, counter, results);
-        return results;
+        solve(n, map, new ArrayList(), ans);
+        return ans;
     }
-
-    protected void backtrack(
-            LinkedList<Integer> comb,
-            Integer N,
-            HashMap<Integer, Integer> counter,
-            List<List<Integer>> results) {
-
-        if (comb.size() == N) {
-            // make a deep copy of the resulting permutation,
-            // since the permutation would be backtracked later.
-            results.add(new ArrayList<Integer>(comb));
+    public void solve(int n, HashMap<Integer, Integer> map, List<Integer> list,List<List<Integer>> ans){
+        if(list.size()==n){
+            ans.add(new ArrayList(list));
             return;
         }
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            int num  =entry.getKey();
+            int value = entry.getValue();
+            
+            if(value==0) continue;
 
-        for (Map.Entry<Integer, Integer> entry : counter.entrySet()) {
-            Integer num = entry.getKey();
-            Integer count = entry.getValue();
-            if (count == 0)
-                continue;
-            // add this number into the current combination
-            comb.addLast(num);
-            counter.put(num, count - 1);
-
-            // continue the exploration
-            backtrack(comb, N, counter, results);
-
-            // revert the choice for the next exploration
-            comb.removeLast();
-            counter.put(num, count);
+            list.add(num);
+            map.put(num, value-1);
+            solve(n, map, list, ans);
+            list.remove(list.size()-1);
+            map.put(num, value);
         }
     }
 }
-
