4dd22f4ed722de2627a1a4c8316127bae66c0ad8
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-12-18 09:49:19 +0600

    Update 377. Combination Sum IV.java

--- leetcode/377. Combination Sum IV.java
+++ leetcode/377. Combination Sum IV.java
@@ -21,3 +21,27 @@ class Solution {
 }
 
 
+//HashMap with int helper
+class Solution {
+    public HashMap<Integer, Integer> map;
+
+    public int combinationSum4(int[] nums, int target) {
+        map = new HashMap();
+        return helper(target, nums, map);
+    }
+    public int helper(int target, int[] nums, HashMap<Integer, Integer> map){
+        if(target==0) return 1;
+        if(map.containsKey(target)) return map.get(target);
+
+        int result = 0;
+        for(int i=0; i<nums.length; i++){
+            if(target-nums[i]>=0)
+                result += helper(target-nums[i], nums, map);
+        }
+        map.put(target, result);
+        return result;
+    }
+}
+
+//
+
