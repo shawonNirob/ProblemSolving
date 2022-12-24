8e28c8968e47487e333521181cc9cc4bd176d3b0
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-12-25 00:28:05 +0600

    Update 5. Longest Palindromic Substring.java

--- leetcode/5. Longest Palindromic Substring.java
+++ leetcode/5. Longest Palindromic Substring.java
@@ -1,3 +1,31 @@
+//TLE Solution
+//Brute Force
+class Solution {
+    public String longestPalindrome(String s) {
+        String str = "";
+        for(int i=0; i<s.length(); i++){
+            for(int j=i;j<s.length(); j++){
+                if(check(s, i, j)){
+                    if(s.substring(i,j+1).length() > str.length()){
+                        str = s.substring(i,j+1);
+                    }
+                }
+            }
+        }
+        return str;
+    }
+    public boolean check(String s, int start, int end){
+        while(start <= end){
+            if(s.charAt(start++) != s.charAt(end--)){
+                return false;
+            }
+        }  
+        return true;
+    }
+}
+
+
+//
 class Solution {
     public String longestPalindrome(String s) {
         if (s == null || s.length() < 1) return "";
