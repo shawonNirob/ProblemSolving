c4438f2f9d13e21cb7f264f16c77c468c3191a8a
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2023-01-07 00:39:33 +0600

    Update 1905. Count Sub Islands.java

--- leetcode/1905. Count Sub Islands.java
+++ leetcode/1905. Count Sub Islands.java
@@ -162,4 +162,47 @@ class Solution {
         return isTrue;
     }
 }
+
+//
+//1. remove all non-common Island
+//2. count the island
+class Solution {
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        int m = grid2.length;
+        int n = grid2[0].length;
+        
+        int result = 0;
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                
+                if(grid1[i][j]==0 && grid2[i][j]==1){
+                    dfs(i, j, grid2, m, n);
+                }
+            }
+        }
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){         
+                if(grid2[i][j]==1){
+                    result++;
+                    dfs(i, j, grid2, m, n);
+                }
+            }
+        }
+        
+        return result;
+    }
+                            
+    public void dfs(int r, int c, int[][] grid2, int m, int n){
+        if(r<0 || r>=m || c<0 || c>=n || grid2[r][c]==0) return;
+        
+        grid2[r][c] = 0;
+        
+        dfs(r+1, c, grid2, m, n);
+        dfs(r-1, c, grid2, m, n);
+        dfs(r, c-1, grid2, m, n);
+        dfs(r, c+1, grid2, m, n);
+    }
+}
 //
