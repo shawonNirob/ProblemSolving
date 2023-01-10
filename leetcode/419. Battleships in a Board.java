b35f63615e94fb49f135f881915981426f99b478
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2023-01-11 03:22:43 +0600

    Update 419. Battleships in a Board.java

--- leetcode/419. Battleships in a Board.java
+++ leetcode/419. Battleships in a Board.java
@@ -27,3 +27,20 @@ class Solution {
 }
 
 //Space --> O(1)
+class Solution {
+    public int countBattleships(char[][] board) {
+        int battleships = 0;
+        
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                if(board[i][j]=='X'){
+                    if(i>0 && board[i-1][j]=='X' || j>0 && board[i][j-1]=='X') continue;
+                    battleships++;
+                }
+            }
+        }
+        
+        return battleships;
+    }
+}
+//
