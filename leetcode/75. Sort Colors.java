42204c2d2a21b6dc22134a0d742547199fe14416
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2023-01-21 04:50:16 +0600

    Update 75. Sort Colors.java

--- leetcode/75. Sort Colors.java
+++ leetcode/75. Sort Colors.java
@@ -1,25 +1,48 @@
-O(n) + O(n)
-
+O(2n)
+//2 pass
 class Solution {
     public void sortColors(int[] nums) {
         int zeros = 0, ones =0, twos =0;
         for(int i=0; i<nums.length; i++){
-            if(nums[i]==0)
-                zeros++;
-            else if(nums[i]==1)
-                ones++;
-            else
-                twos++;
+            if(nums[i]==0) zeros++;
+            else if(nums[i]==1) ones++;
+            else twos++;
         }
         for(int i=0; i<nums.length; i++){
-            if(i < zeros)
-                nums[i] = 0;
-            else if(i < zeros+ones)
-                nums[i] = 1;
-            else
-                 nums[i] = 2;
+            if(i < zeros) nums[i] = 0;
+            else if(i < zeros+ones) nums[i] = 1;
+            else nums[i] = 2;
         }
     }
 }
+
+O(n)
+//1 pass
+class Solution {
+    public void sortColors(int[] nums) {
+        int red=0, white=0, blue = nums.length-1;
+        
+        while(white <= blue){
+            if(nums[white]==0){
+                swap(nums, white, red);
+                red++;
+                white++;
+            }
+            else if(nums[white] == 1){
+                white++;
+            }
+            else if(nums[white] == 2){
+                swap(nums, white, blue);
+                blue--;
+            }
+        }
+    }
+    public void swap(int[] nums, int n1, int n2){
+        int temp = nums[n1];
+        nums[n1] = nums[n2];
+        nums[n2] = temp;
+    }
+}
+
 
 
