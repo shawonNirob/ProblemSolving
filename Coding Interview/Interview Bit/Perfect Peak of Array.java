963e966d46fbd5b8e02b86cb26885db457e71f3e
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2023-07-28 22:51:51 +0600

    Update Perfect Peak of Array.java

--- Coding Interview/Interview Bit/Perfect Peak of Array.java
+++ Coding Interview/Interview Bit/Perfect Peak of Array.java
@@ -1,3 +1,32 @@
+//What is the Maximum in Left and Minimum in right
+public class Solution {
+    public int perfectPeak(int[] A) {
+        //O(n)
+        int n = A.length;
+        
+        int[] maxLeft = new int[n];
+        int[] minRight = new int[n]; 
+        
+        maxLeft[0] = A[0];
+        minRight[n-1] = A[n-1];
+        for(int i=1; i<n-1; i++){
+            maxLeft[i] = Math.max(maxLeft[i-1], A[i]);
+            minRight[n-1-i] = Math.min(A[n-1-i], minRight[n-i]); //minRight[n-1-i+1]
+        }
+        
+        for(int i=1; i<n-1; i++){
+            if(A[i] > maxLeft[i-1] && A[i] < minRight[i+1]){
+                return 1;
+            }
+        }
+        
+        return 0;
+    }
+}
+//
+
+
+//Brute Force
 public class Solution {
     public int perfectPeak(int[] A) {
         //Brute Force
